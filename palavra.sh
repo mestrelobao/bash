9409973191e2e902b4a689090ecea569e0aa3460
mestrelobao <mestre_lobao@yahoo.com>
2026-01-07 19:51:26 -0300

    Create palavra.sh
    seleciona palavras de uma determinada linha de um texto.txt

---
+++ palavra.sh
@@ -0,0 +1,5 @@
+#selecionar palavras no texto
+#!/bin/bash
+mapfile -t myArray < mare.txt
+mapfile -t -d ' ' fieldsArray << "$myArray[1]}"
+echo " a primeira palavra da linha 2 é: ${fieldsArray[0]}"
